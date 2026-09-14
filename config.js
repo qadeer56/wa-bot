fc1880c7ba277101e961c88171b461d550acee55
Muhammad Qadeer Khan <qk179693@gmail.com>
2026-09-14 11:43:51 +0500

    Fix PUPPETEER_EXECUTABLE_PATH assignment

--- config.js
+++ config.js
@@ -5,7 +5,10 @@ module.exports = {
   OWNER_NAME: process.env.OWNER_NAME || 'Owner',
   BOT_NAME: process.env.BOT_NAME || 'Bot',
   PREFIX: process.env.PREFIX || '.',
+
   ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,
   WEATHER_API_KEY: process.env.WEATHER_API_KEY,
-  PUPPETEER_EXECUTABLE_PATH: process.env.PUPPETEER_EXECUTABLE_PATH,
+
+  PUPPETEER_EXECUTABLE_PATH:
+    process.env.PUPPETEER_EXECUTABLE_PATH,
 };
